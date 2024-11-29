7f7afe365bbe3e970f21502fb9bf934c37408214
Ranjith K <thisisranjithk@gmail.com>
2024-11-29 13:46:40 +0530

    boilerplate clearup & MetaData

---
+++ constants.ts
@@ -0,0 +1,19 @@
+export const METADATA = {
+  authors: [{ name: "Ranjith K" }],
+  title: "Weatherly",
+  description:
+    "Weatherly is your go-to weather dashboard, offering accurate and real-time weather updates, hourly and weekly forecasts, and detailed insights for locations worldwide. Stay prepared with Weatherly.",
+  keywords: [
+    "Weatherly",
+    "Weather Dashboard",
+    "Real-Time Weather",
+    "Accurate Weather Forecast",
+    "Geolocation Weather",
+    "Search Weather",
+    "Weather Insights",
+  ].join(", "),
+  creator: "Ranjith K",
+  publisher: "Ranjith K",
+  language: "English",
+  themeColor: "#ffffff",
+};
