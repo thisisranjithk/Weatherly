49eb789491122036c679362f62af9c047f348d78
Ranjith K <thisisranjithk@gmail.com>
2024-12-10 13:12:31 +0530

    searchInput placeholder update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   return (
     <section className="h-screen w-screen flex pt-44 justify-center">
       <SearchInput
-        placeholder="Enter a city..."
+        placeholder="Enter a location.."
         classes="p-4 text-2xl md:text-4xl "
         cloudIconClasses="text-2xl md:text-4xl right-7 top-5"
       />
