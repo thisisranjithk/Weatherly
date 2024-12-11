95c64dffe44e21a55f2040e367dd58674af83be1
Ranjith K <thisisranjithk@gmail.com>
2024-12-11 13:30:10 +0530

    searchInput error handling

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ import { Suspense } from "react";
 export default async function Home() {
   return (
     <section className="h-screen w-screen flex pt-44 justify-center">
-      <Suspense fallback={<div>Loading...</div>}>
         <SearchInput
           placeholder="Enter a location.."
           classes="p-4 text-2xl md:text-4xl "
           cloudIconClasses="text-2xl md:text-4xl right-7 top-5"
         />
-      </Suspense>
     </section>
   );
 }
--- components/Common/SearchInput.tsx
+++ components/Common/SearchInput.tsx
@@ -15,6 +15,7 @@ const SearchInput = ({
   cloudIconClasses,
 }: SearchInputProps) => {
   const [queryTerm, setQueryTerm] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
   const searchparams = useSearchParams();
   const params = new URLSearchParams(searchparams);
   const router = useRouter();
@@ -28,29 +29,33 @@ const SearchInput = ({
   const handleClick = () => {
     if (queryTerm.trim()) {
       params.set("q", queryTerm.trim());
-      router.push(`/weather?q=${encodeURIComponent(queryTerm)}`);
+      router.push(`/weather?q=${encodeURIComponent(queryTerm.trim())}`);
     } else {
-      alert("Enter the valid location");
+      if (queryTerm.trim() === "") {
+        setError("Please, Enter the location");
+      }
     }
-
     setQueryTerm("");
   };
 
   return (
-    <div className="relative">
-      <input
-        className={`${classes} bg-transparent text-primary border-[2.5px] font-bold rounded-md placeholder:text-primary border-primary active:border-none outline-none`}
-        type="text"
-        value={queryTerm}
-        placeholder={placeholder}
-        onChange={(e) => setQueryTerm(e.target.value)}
-        onKeyUp={(e) => handleKeyPress(e.key)}
-      />
+    <div className="flex flex-col">
+      <div className="relative">
+        <input
+          className={`${classes} bg-transparent text-primary border-[2.5px] font-bold rounded-md placeholder:text-primary border-primary active:border-none outline-none`}
+          type="text"
+          value={queryTerm}
+          placeholder={placeholder}
+          onChange={(e) => setQueryTerm(e.target.value)}
+          onKeyUp={(e) => handleKeyPress(e.key)}
+        />
 
-      <FaCloudversify
-        onClick={handleClick}
-        className={`${cloudIconClasses} absolute top-3 text-primary hover:text-[#071952] right-3 duration-200 `}
-      />
+        <FaCloudversify
+          onClick={handleClick}
+          className={`${cloudIconClasses} absolute top-3 text-primary hover:text-[#071952] right-3 duration-200 `}
+        />
+      </div>
+      {error && <p className="font-bold text-red-600 mt-2 ml-2">{error}</p>}
     </div>
   );
 };
